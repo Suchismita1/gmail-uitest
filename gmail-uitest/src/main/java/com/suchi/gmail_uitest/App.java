558020f5d4a5eef3dc314ac3cbab984cfc34f43d
Suchi <Suchi@LAPTOP-O50P61C6>
2016-11-26 18:34:03 +0530

    committing 2nd trail

---
+++ gmail-uitest/src/main/java/com/suchi/gmail_uitest/App.java
@@ -0,0 +1,13 @@
+package com.suchi.gmail_uitest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
